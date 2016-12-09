05806a82326437668b14158a764ce115472a0f89
jasonwang <jassonay624@gmail.com>
2016-12-09 13:14:53 +0800

    Initial commit

---
+++ app/src/main/java/com/example/user/myapplication1209_2/FragmentA.java
@@ -0,0 +1,54 @@
+package com.example.user.myapplication1209_2;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentA extends Fragment {
+    Button btn2;
+
+    public FragmentA() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v =inflater.inflate(R.layout.fragment_a, container, false);
+        final TextView tv = (TextView) v.findViewById(R.id.textView);
+        Button btn = (Button) v.findViewById(R.id.button);
+        btn2 = (Button) v.findViewById(R.id.button2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText("Hello Word!");
+            }
+        });
+
+        return v;
+    }
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+        final TextView tv3 = (TextView) getActivity().findViewById(R.id.textView3);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv3.setText("Hello 3333");
+            }
+    });
+    }
+
+}
